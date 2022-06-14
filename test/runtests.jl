d1d9c09dbac688ce2ae0a1c34d4a77b7225f6866
WooKyoung Noh <wookay.noh@gmail.com>
2022-06-14 12:11:51 +0900

    testset filter using Jive

---
+++ test/runtests.jl
@@ -0,0 +1,4 @@
+using Jive
+runtests(@__DIR__, testset="hello")
+
+runtests(@__DIR__, testset=r"llo$")
---
+++ test/test1.jl
@@ -0,0 +1,9 @@
+using Test
+
+@testset "hello" begin
+@test true
+end
+
+@testset "world" begin
+@test false
+end
